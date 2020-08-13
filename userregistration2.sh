a127793aa7ef7eb3cf8744536765aaddffb5b1e2
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-13 11:48:47 +0530

    UC3added

--- userregistration2.sh
+++ userregistration2.sh
@@ -2,9 +2,20 @@
 
 read -p "Ennter your firstname: " fname
 read -p "Enter your last name: " lname
+read -p "Enter your your email: " mail
 
 firstname="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
 lastname="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+email="^([a-zA-z_]{1,}[.-+_]?[a-zA-z0-9]+)\@([a-z0-9]+)\.([a-z]{2,5})$"
+email1="^([a-zA-z_]{1,}[.-+_]?[a-zA-z0-9]+)\@([a-z0-9]+)\.([a-z]{2,5})\.([a-z]{2,5})$"
+
+
+if [[ $lname =~ $lastname ]]
+then
+   echo "Valid lastname"
+else
+   echo "Invalid lastname"
+fi
 
 if [[ $fname =~ $firstname ]]
 then
@@ -14,9 +25,9 @@ else
 fi
 
 
-if [[ $lname =~ $lastname ]]
+if [[ $mail =~ $email || $mail =~ $email1 ]]
 then
-	echo "Valid lastname"
+	echo "Valid email"
 else
-	echo "Invalid lastname"
+	echo "Invalid email"
 fi
