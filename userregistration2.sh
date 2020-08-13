a69d959fcd37794923b28b6200a187d2d750fc73
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-13 11:29:55 +0530

    UC1-added

--- userregistration2.sh
+++ userregistration2.sh
@@ -1,2 +1,12 @@
 #!/bin/bash -x
 
+read -p "Ennter your firstname: " fname
+
+firstname="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+
+if [[ $fname =~ $firstname ]]
+then
+	echo "Valid first name"
+else
+	echo "Invalid first name"
+fi
