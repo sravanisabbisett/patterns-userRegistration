4f8cc68aa1edca0c8c0cc49237b8a56ef506a4b9
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-13 11:36:22 +0530

    UC2 added

--- userregistration2.sh
+++ userregistration2.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -x
 
 read -p "Ennter your firstname: " fname
+read -p "Enter your last name: " lname
 
 firstname="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
+lastname="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
 
 if [[ $fname =~ $firstname ]]
 then
-	echo "Valid first name"
+   echo "Valid first name"
 else
-	echo "Invalid first name"
+   echo "Invalid first name"
+fi
+
+
+if [[ $lname =~ $lastname ]]
+then
+	echo "Valid lastname"
+else
+	echo "Invalid lastname"
 fi
