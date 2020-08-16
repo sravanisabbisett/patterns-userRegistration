02dd41f38575d220802b65ee2f8a2f4b3f515d1b
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-08-16 18:39:30 +0530

    added

--- userregistration2.sh
+++ userregistration2.sh
@@ -11,35 +11,18 @@ lastname="^([[:upper:]]{1})([[:lower:]]{2})([a-z]*)$"
 email="^([a-zA-z_]{1,}[.-+_]?[a-zA-z0-9]+)\@([a-z0-9]+)\.([a-z]{2,5})$"
 email1="^([a-zA-z_]{1,}[.-+_]?[a-zA-z0-9]+)\@([a-z0-9]+)\.([a-z]{2,5})\.([a-z]{2,5})$"
 phonenumber="([0-9]{2})[: :][6-9]{1}[0-9]{9}"
-password="(.*[[:upper:]].*)(.*[[:lower:]].*)(.*[0-9].*)([@$%&]{1})"
+password="^([[:upper:]]{1}[[:lower:]]{4,}[0-9]{2,}[@#$^]{1})$"
 
 
-if [[ $fname =~ $firstname ]]
+if [[ $fname =~ $firstname && $lname =~ $lastname && $number =~ $phonenumber && $pass =~ $password ]]
 then
-   if [[ $lname =~ $lastname ]]
+   if [[ $mail =~ $email || $mail =~ $email1 ]]
    then
-      if [[ $mail =~ $email || $mail =~ $email1 ]]
-      then
-         if [[ $number =~ $phonenumber ]]
-         then
-            if [[ ${#pass} -ge 8 ]]
-            then
-               if [[ $pass =~ $password ]]
-               then
-                  echo "user registered sucessfully"
-               fi
-            else
-               echo "please Enter valid password"
-            fi
-         else
-            echo "please enter valid mobile number"
-         fi
-      else
-         echo "Enter valid user email id"
-      fi
+      echo "user registered sucessfully"
    else
-      echo  "Enter valid lastname"
+      echo "Please enter valid details"
    fi
 else
-   echo "Enter valid firstname"
+   echo "Please enter valid details"
 fi
+
